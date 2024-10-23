0d1b0fe40c6f6d68e11d11bbf13cdb889178b7fb
avetik <avetiq1992avo@gmail.com>
2024-10-23 10:47:11 -0700

    c++ methods

--- addon.cpp
+++ addon.cpp
@@ -5,6 +5,7 @@ using v8::Isolate;
 using v8::String;
 using v8::Value;
 using v8::Local;
+using v8::Number;
 
 void Hello(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
@@ -12,8 +13,21 @@ void Hello(const FunctionCallbackInfo<Value>& args) {
         isolate, "hello world").ToLocalChecked());
 }
 
+void Sum(const FunctionCallbackInfo<Value>& args) {
+    v8::Isolate* isolate = args.GetIsolate();
+    int i;
+    double a = 5.4563, b = 9.5879;
+    for(i = 0;i < 1000000000; i++ ) {
+        a +=b;
+    }
+
+    auto total = v8::Number::New(isolate, a);
+    args.GetReturnValue().Set(total);
+}
+
 void Initialize(v8::Local<v8::Object> exports) {
     NODE_SET_METHOD(exports, "hello", Hello);
+    NODE_SET_METHOD(exports, "sum", Sum);
 }
 
 NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 
 const addon = require('./build/Release/addon')
 
-console.log(addon.hello())
+function jsSum(){
+    let a = 5.4563
+    let b = 9.5879
+
+    for(let i = 0;i < 1000000000; i++ ) {
+        a += b;
+    }
+    let total = a;
+    return total;
+}
+
+console.time('c++')
+console.log(addon.sum())
+console.timeEnd('c++')
+
+console.time('Js')
+console.log(jsSum())
+console.timeEnd('Js')
